88b07bad48fb02761a67dd3d8a4ecaa0328c54bd
HerbertHe <Herbert.He0229@gmail.com>
2020-03-15 14:25:13 +0800

    基本完善Edever图形客户端

--- src/components/help.js
+++ src/components/help.js
@@ -7,7 +7,7 @@ function Help(props) {
         <div className="Help">
             <div className="container">
                 <div className="help-header">
-                    <div style={{ color: "white" }}>Help</div>
+                    <div style={{ color: "white" }}>Help & About</div>
                     <div onClick={() => props.control(false)}>
                         <IconFont
                             type="icon-close"
@@ -19,7 +19,16 @@ function Help(props) {
                     </div>
                 </div>
                 <div className="help-body">
-
+                    <div style={{ color: "#3c40c6" }}>Edever Client</div>
+                    <div>一个跟踪electron系列框架动态的客户端...</div>
+                    <br />
+                    <div>Author: Herbert He</div>
+                    <div>Email: admin@jieec.cn / Herbert.He0229@gmail.com</div>
+                    <div>Blog: https://goer.icu</div>
+                    <div>Version: v1.0.0</div>
+                    <div>
+                        Issues: https://github.com/HerbertHe/EdeverClient/issues
+                    </div>
                 </div>
             </div>
         </div>
